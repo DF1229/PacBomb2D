7bbeaaf8bb847749202b9eca73383bbbba5cef5c
DF1229 <daanfaber1@gmail.com>
2021-11-23 13:00:27 +0100

    Changed lives to display sprites instead of text

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,10 +4,14 @@ using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject[] lives;
+    public Image life1;
+    public Image life2;
+    public Image life3;
+
     public GameObject scoreValue;
     public GameObject attackDisplay;
     private TextMeshProUGUI score;
+    private Image[] lives = new Image[3];
     
     private static UIManager _instance;
     public static UIManager Instance
@@ -47,6 +51,34 @@ public class UIManager : MonoBehaviour
 
     public void UpdateLives(int lives)
     {
-        
+        switch (lives)
+        {
+            case 0:
+                life1.gameObject.SetActive(false);
+                life2.gameObject.SetActive(false);
+                life3.gameObject.SetActive(false);
+                break;
+            case 1:
+                life1.gameObject.SetActive(true);
+                life2.gameObject.SetActive(false);
+                life3.gameObject.SetActive(false);
+                break;
+            case 2:
+                life1.gameObject.SetActive(true);
+                life2.gameObject.SetActive(true);
+                life3.gameObject.SetActive(false);
+                break;
+            case 3:
+                life1.gameObject.SetActive(true);
+                life2.gameObject.SetActive(true);
+                life3.gameObject.SetActive(true);
+                break;
+            default:
+                life1.gameObject.SetActive(true);
+                life2.gameObject.SetActive(false);
+                life3.gameObject.SetActive(true);
+                Debug.Log("UpdateLives() default case reached");
+                break;
+        }
     }
 }
